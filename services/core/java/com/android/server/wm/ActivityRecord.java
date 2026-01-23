a00adfdbbf51436778bc87da4b942c2efd0287bd
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-01-23 09:07:11 +0900

    fixup! fixing force full screen feature
    Change-Id: Iae015cd4b5627c47fb0f1b0cb1e3e24e77f0834e

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -9712,10 +9712,6 @@ final class ActivityRecord extends WindowToken {
     }
 
     public boolean shouldForceLongScreen() {
-        return shouldForceLongScreen(info.packageName);
-    }
-
-    public boolean shouldForceLongScreen(String pkg) {
-        return mAtmService.shouldForceLongScreen(pkg);
+        return mAtmService.shouldForceLongScreen(packageName);
     }
 }
