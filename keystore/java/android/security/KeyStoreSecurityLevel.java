Neoteric-OS/android_frameworks_base

base: Add key generation for keybox spoofing [1/2]
The code is used from :-
https://github.com/5ec1cff/TrickyStore

This allows us to enjoy spoofing keyboxes the right way even with broken TEE.
Allow users to provide keybox XML as well.

Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

keystore/java/android/security/KeyStoreSecurityLevel.java
@@ -33,6 +33,8 @@ import android.system.keystore2.KeyMetadata;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
+import com.android.internal.util.neoteric.KeyboxImitationHooks;
+
 import java.util.Calendar;
 import java.util.Collection;
 
@@ -145,6 +147,15 @@ public class KeyStoreSecurityLevel {
             Collection<KeyParameter> args, int flags, byte[] entropy)
             throws KeyStoreException {
         StrictMode.noteDiskWrite();
+
+        KeyboxImitationHooks.setSuccessFlag(false);
+        if (attestationKey == null) {
+            KeyMetadata metadata = KeyboxImitationHooks.generateKey(mSecurityLevel,
+                    descriptor, args);
+            if (metadata != null) {
+                return metadata;
+            }
+        }
 
         return handleExceptions(() -> mSecurityLevel.generateKey(
                 descriptor, attestationKey, args.toArray(new KeyParameter[args.size()]),

DarkDolphin1/evo_frameworks_base

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

keystore/java/android/security/KeyStoreSecurityLevel.java
@@ -33,6 +33,8 @@ import android.system.keystore2.KeyMetadata;
 import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
+import com.android.internal.util.evolution.KeyboxImitationHooks;
+
 import java.util.Calendar;
 import java.util.Collection;
 
@@ -145,6 +147,15 @@ public class KeyStoreSecurityLevel {
             Collection<KeyParameter> args, int flags, byte[] entropy)
             throws KeyStoreException {
         StrictMode.noteDiskWrite();
+
+        KeyboxImitationHooks.setSuccessFlag(false);
+        if (attestationKey == null) {
+            KeyMetadata metadata = KeyboxImitationHooks.generateKey(mSecurityLevel,
+                    descriptor, args);
+            if (metadata != null) {
+                return metadata;
+            }
+        }
 
         return handleExceptions(() -> mSecurityLevel.generateKey(
                 descriptor, attestationKey, args.toArray(new KeyParameter[args.size()]),